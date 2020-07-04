615cad166617e6efe681da53057c5c7fd29bad2c
Anthony Pogliano <poglianoster@gmail.com>
2020-07-04 12:29:59 -0600

    Made post preview component for ease of styling. Removed unused declarations in some files. Roughed in some styling for post previews.

---
+++ content/blog/fourth-post/index.md
@@ -0,0 +1,22 @@
+---
+title: Fourth Post
+date: '2020-07-01T22:12:03.284Z'
+description: 'Here is a manaully entered description. It will work for letting people know that the content of this blog post is bullshit.'
+author: 'Anthony Pogliano'
+slug: 'fourth-post'
+---
+
+## Fourth Post
+
+Here's some shit in the post
+
+```js
+function test(param) {
+  return (`Here's some thing with ${param`)
+}
+
+```
+
+### Stuff
+
+More stuff
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { rhythm } from 'typography'
 import usePosts from '../hooks/use-posts'
-import { Link } from 'gatsby'
-// import PostPreview from '../components/post-preview'
+import PostPreview from '../components/post-preview'
 
 const Blog = ({ location }) => {
   const posts = usePosts()
@@ -13,31 +11,9 @@ const Blog = ({ location }) => {
     <Layout title='Blog Posts' location={location}>
       <SEO title='Blog Posts' />
       <h1>This will be where all blog posts are listed</h1>
-
-      {posts.map(post => {
-        console.log('Post inside map: ', post)
-        const title = post.title || post.slug
-        return (
-          <article key={post.slug}>
-            <header>
-              <h3>
-                <Link style={{ boxShadow: `none` }} to={post.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <time>{post.date}</time>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: post.description
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
     </Layout>
   )
 }
