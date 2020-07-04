5c7412d05d7c0af97ddae6e1abe752b10992f1dc
Anthony Pogliano <poglianoster@gmail.com>
2020-07-03 18:49:49 -0600

    Reconfiguring blog setup. Blog postURLS are now relative to the  directory. Removed some more unused assets (.ts related stuff that's not being used). Created a hook to populate blog posts so that I can separate component from queries.

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,35 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              description
+              slug
+              author
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return data.allMdx.edges.map(post => ({
+    title: post.node.frontmatter.title,
+    author: post.node.frontmatter.author || 'Anthony Pogliano',
+    date: post.node.frontmatter.date,
+    description: post.node.frontmatter.description || post.node.excerpt,
+    slug: post.node.frontmatter.slug || post.node.fields.slug
+  }))
+}
+
+export default usePosts
