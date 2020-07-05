5952b869ea43fe5b082655a83c0a0c1d6b5d1fb9
Anthony Pogliano <poglianoster@gmail.com>
2020-07-05 13:51:52 -0600

    Styling of post-preview component imiplies that entire rendered component is clickable link. Updated component so that it functions in accordance with its stylinig.

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -4,50 +4,52 @@ import { css } from '@emotion/core'
 
 const PostPreview = ({ post }) => {
   return (
-    <article
-      key={post.slug}
+    <Link
+      to={post.slug}
       css={css`
+              color: inherit;
+              text-decoration: none;
+            `}
+    >
+      <article
+        key={post.slug}
+        css={css`
         margin-bottom: 2rem;
         border-radius: 1px;
         padding: 1rem;
         background-color: dodgerblue;
         box-shadow: -3px 2px .25rem red;
       `}
-    >
-      <header
-        css={css`
+      >
+        <header
+          css={css`
           display: flex;
           justify-content: space-between;
         `}
-      >
-        <h3>
-          <Link 
-            to={post.slug}
-            css={css`
-              color: inherit;
-              text-decoration: none;
-            `}  
-          >
+        >
+          <h3>
+
             {post.title}
-          </Link>
-        </h3>
-        <time
-          css={css`
+
+          </h3>
+          <time
+            css={css`
             display: inline-block;
             margin-bottom: .75rem;
           `}
-        >
-          {post.date}
-        </time>
-      </header>
-      <section>
-        <p
-          dangerouslySetInnerHTML={{
-            __html: post.description
-          }}
-        />
-      </section>
-    </article>
+          >
+            {post.date}
+          </time>
+        </header>
+        <section>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: post.description
+            }}
+          />
+        </section>
+      </article>
+    </Link>
   )
 }
 
