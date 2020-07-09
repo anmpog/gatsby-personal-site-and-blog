37160f019d9d5272ecacfa18c0364698a664f698
Anthony Pogliano <poglianoster@gmail.com>
2020-07-09 16:28:52 -0600

    Starting to implement fonts. Some style changes, mostly font related.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/src/images`,
         name: `images`,
       },
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,31 @@
 import Typography from 'typography'
 
-const typography = new Typography()
+const typography = new Typography({
+  title: `anmpogSiteTheme`,
+  // baseFontSize: `20px`,
+  googleFonts: [
+    {
+      name: `Bungee`,
+      styles: [
+        `400`
+      ]
+    },
+    {
+      name: `Open Sans`,
+      styles: [
+        `300`,
+        `300i`,
+        `400`,
+        `400i`,
+        `600`
+      ]
+    }
+  ],
+    headerFontFamily: ['Bungee', 'sans-serif'],
+    bodyFontFamily: ['Open Sans', 'sans-serif'],
+    bodyWeight: 300,
+    includeNormalize: false,
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
