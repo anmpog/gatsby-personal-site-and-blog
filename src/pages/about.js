fc6bc4ef780ebff2f0fdc5fd9b904bca487bf4eb
Anthony Pogliano <poglianoster@gmail.com>
2020-07-17 16:19:07 -0600

    Final edit of about page before deploy. I genuinely hate writing about myself. Still needs styling for lists.

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,16 +8,36 @@ const About = ({ location }) => {
     <SEO title='About Me' location={location} />
       <div>
         <p>
-          I'm Anthony! I'm a self-taught web developer who lives in Boulder, Colorado. My coding journey started when I was forced to use a really crappy piece of software every day and I thought, "Hmmm. I could do a better job of making an app than this..." It turns out that the joke is on me: it's not that easy to make applications.
+          I'm Anthony! I'm a self-taught web developer who lives in Boulder, Colorado. I am primarily a JavaScript developer, and I gravitate towards front-end development. Please feel free to contact me on my social media if you want to chat about anything! Also, please 🐻 with me while I develop this site! 
         </p>
 
         <p>
-          I'm a fairly well-adjusted person outside my interest in coding. I enjoy climbing, skiing, bicycles, cooking, video games, watches, kitty cats (and animals in general... but cats are the dopest animals), unironically using skier-bro lingo from the early 2000s... you probably get the picture.
+          Outside coding, my interests include climbing, skiing, bicycles, cooking, making/consuming music, video games, watches, kitty cats, unironically using skier-bro lingo from the early 2000s... you probably get the picture.
         </p>
 
         <p>
-          I strongly believe that our professional endeavors should be informed and guided by our ethical beliefs. I was initially drawn to coding in part because it felt like a "democratic" skillset. I want to be involved in projects that help people and make the world a better place - not just things that improve a company's bottom line.
+          I strongly believe that our professional endeavors should be informed and guided by our ethical beliefs. I am happiest when I am involved in projects that help people and make the world a better place - not just things that improve a company's bottom line. Black Lives Matter.
         </p>
+
+        <h3>Tech</h3>
+        <ul>
+          <li>JavaScript ES6</li>
+          <li>NodeJS</li>
+          <li>React</li>
+          <li>CSS3</li>
+          <li>HTML5</li>
+          <li>MongoDB</li>
+          <li>GraphQL</li>
+        </ul>
+
+        <h3>Tools</h3>
+        <ul>
+          <li>GatsbyJS</li>
+          <li>VSCode</li>
+          <li>Postman</li>
+          <li>Robo 3T</li>
+          <li>Firefox</li>
+        </ul>
       </div>
     </Layout>
   )
